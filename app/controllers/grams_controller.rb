9863be9ad8c6e3ce07d813837b3abbe2af919fa2
TerrenceBeckham <terrencebeckham@icloud.com>
2017-06-03 14:09:03 +0200

    Added Devise

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,19 +1,8 @@
 class GramsController < ApplicationController
-  def new
-  @gram = Gram.new
-  end
-
   def index
   end
 
-  def create
-    @gram = Gram.create(gram_params)
-    redirect_to root_path
-  end
-
-  private
-
-  def gram_params
-    params.require(:gram).permit(:message)
+  def new
+  @gram = Gram.new
   end
 end
