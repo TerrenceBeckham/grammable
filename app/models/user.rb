cca2339f96daccbbc18251d307042402a2b1795f
TerrenceBeckham <terrencebeckham@icloud.com>
2017-06-05 11:31:12 +0200

    Connected grams to users and tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :grams  #This sets up the relationship between user and gram
 end
