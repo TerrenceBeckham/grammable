ea3eff7fff39d542038af6bb84bdc76f8a8133aa
TerrenceBeckham <terrencebeckham@icloud.com>
2017-06-11 16:40:14 +0200

    Fixed bug in index /view

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -23,7 +23,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-   process resize_to_fit: [200, 300]
+   #process resize_to_fit: [200, 300]
 
   #
   # def scale(width, height)
